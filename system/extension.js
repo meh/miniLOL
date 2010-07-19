1649efed9aeda10cd67f639406e967ffd9969331
meh <meh.ffff@gmail.com>
2010-07-19 11:11:51 +0200

    Small improvements.

--- system/extension.js
+++ system/extension.js
@@ -29,96 +29,128 @@ else {
     }
 }
 
-Object.isBoolean = function (val) {
-    return val.constructor === Boolean;
-};
+Object.extend(Object, {
+    isBoolean: function (val) {
+        return val.constructor === Boolean;
+    },
 
-String.fromAttributes = function (attributes) {
-    var result = '';
+    fromAttributes: function (attributes) {
+        var result = {};
+
+        for (var i = 0; i < attributes.length; i++) {
+            result[attributes.item(i).nodeName] = attributes.item(i).nodeName;
+        }
+
+        return result;
+    },
+
+    toQuery: function (query) {
+        var result = '';
     
-    for (var i = 0; i < attributes.length; i++) {
-        result += '#{name}="#{value}" '.interpolate({
-            name: attributes.item(i).nodeName,
-            value: attributes.item(i).nodeValue
-        });
+        for (var name in query) {
+            result += "#{name}=#{value}&".interpolate({
+                name: name,
+                value: query[name]
+            });
+        }
+    
+        return result.substr(0, result.length - 1);
     }
+});
+
+Object.extend(String, {
+    fromAttributes: function (attributes) {
+        var result = '';
     
-    return result;
-};
-
-Object.fromAttributes = function (attributes) {
-    var result = {};
-
-    for (var i = 0; i < attributes.length; i++) {
-        result[attributes.item(i).nodeName] = attributes.item(i).nodeName;
-    }
-
-    return result;
-};
-
-String.prototype.parseQuery = function () {
-    var result  = {};
-    var matches = this.match(/[?#](.*)$/);
+        for (var i = 0; i < attributes.length; i++) {
+            result += '#{name}="#{value}" '.interpolate({
+                name: attributes.item(i).nodeName,
+                value: attributes.item(i).nodeValue
+            });
+        }
     
-    if (!matches) {
         return result;
     }
-    
-    var blocks = matches[1].split(/&/);
-    for (var i = 0; i < blocks.length; i++) {
-        var parts = blocks[i].split(/=/);
-        var name  = decodeURIComponent(parts[0]);
+})
+
+Object.extend(String.prototype, {
+    parseQuery: function () {
+        var result  = {};
+        var matches = this.match(/[?#](.*)$/);
         
-        if (parts[1]) {
-            result[name] = decodeURIComponent(parts[1]);
+        if (!matches) {
+            return result;
         }
-        else {
-            result[name] = true;
+        
+        var blocks = matches[1].split(/&/);
+        for (var i = 0; i < blocks.length; i++) {
+            var parts = blocks[i].split(/=/);
+            var name  = decodeURIComponent(parts[0]);
+            
+            if (parts[1]) {
+                result[name] = decodeURIComponent(parts[1]);
+            }
+            else {
+                result[name] = true;
+            }
         }
-    }
-    
-    return result;
-}
+        
+        return result;
+    },
 
-Object.toQuery = function (query) {
-    var result = '';
+    isURL: function () {
+        var match = this.match(/^mailto:([\w.%+-]+@[\w.]+\.[A-Za-z]{2,4})$/);
+        if (match) {
+            return {
+                protocol: "mailto",
+                uri:      match[1]
+            };
+        }
     
-    for (var name in query) {
-        result += "#{name}=#{value}&".interpolate({
-            name: name,
-            value: query[name]
-        });
-    }
+        match = this.match(/^(\w+):(\/\/.+?(:\d)?)(\/)?/);
     
-    return result.substr(0, result.length - 1);
-}
-
-String.prototype.isURL = function () {
-    var match = this.match(/^mailto:([\w.%+-]+@[\w.]+\.[A-Za-z]{2,4})$/);
-    if (match) {
+        if (!match) {
+            return false;
+        }
+    
         return {
-            protocol: "mailto",
-            uri:      match[1]
+            protocol: match[1],
+            uri:      match[2]
         };
     }
+});
 
-    match = this.match(/^(\w+):(\/\/.+?(:\d)?)(\/)?/);
+Element.addMethods({
+    load: function (path, options) {
+        if (options && !Object.isUndefined(options.frequency)) {
+            new Ajax.PeriodicalUpdater(this, path, options);
+        }
+        else {
+            new Ajax.Updater(this, path, options);
+        }
+    },
 
-    if (!match) {
-        return false;
+    getTextDescendants: function (element) {
+        var result = [];
+        
+        function accumulateTextChildren (parent) {
+            var child = parent.firstChild;
+            
+            while (child) {
+                if (Node.TEXT_NODE == child.nodeType) {
+                    result.push(child);
+                }
+                
+                if (Object.isElement(child)) {
+                    accumulateTextChildren(child);
+                }
+                
+                child = child.nextSibling;
+            }
+        }
+        
+        accumulateTextChildren(element);
+        
+        return result;
     }
-
-    return {
-        protocol: match[1],
-        uri:      match[2]
-    };
-}
-
-Element.prototype.load = function (path, options) {
-    if (options && !Object.isUndefined(options.frequency)) {
-        new Ajax.PeriodicalUpdater(this, path, options);
-    }
-    else {
-        new Ajax.Updater(this, path, options);
-    }
-}
+});
